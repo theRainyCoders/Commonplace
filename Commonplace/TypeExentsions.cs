84651eb60aa8e1ee1d8b255cc2e4aac874ce284f
Joerg Bernhard (#JB4) <bernhard.joerg@googlemail.com>
2020-03-29 18:08:13 +0200

    initial commit

---
+++ Commonplace/TypeExentsions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace TheRainyCoders.Commonplace
+{
+    public static class TypeExentsions
+    {
+        public static string GetAssemblyFileVersion(this object instance)
+        {
+            var assembly = Assembly.GetAssembly(instance.GetType());
+
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fvi.FileVersion;
+        }
+
+        public static string GetAssemblyInformationalVersion(this object instance)
+        {
+            var attribute = Assembly.GetAssembly(instance.GetType())
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            return attribute.InformationalVersion;
+        }
+    }
+}
