dd06c3a551e3c0a700eb8077d5f3642d39040627
Joerg Bernhard (#JB4) <bernhard.joerg@googlemail.com>
2020-10-15 22:01:27 +0200

    added test project, set all to .net50

---
+++ Commonplace.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TheRainyCoders.Commonplace.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
